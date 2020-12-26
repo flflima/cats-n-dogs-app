857d61e2cbf64cec9dee2e557447b790589b3058
Felipe Lima <flf.felipe@gmail.com>
2020-12-26 15:03:04 -0300

    Redesenha pagina

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/cats-n-dogs-app'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/cats-n-dogs-app/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/cats-n-dogs-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
